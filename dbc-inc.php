21f5278653a6d8779fec135f0aba959d522b78a3
Shubh Patel <shubhpatel330@gmail.com>
2023-12-01 11:20:28 -0500

    PHP configed

--- dbc-inc.php
+++ dbc-inc.php
@@ -1,15 +1,34 @@
-<?php 
+<?php
 
-$dsn = "mysql:host=localhost;dbname=sys";
-$dbusername = "root";
-$dbpassword = "";
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
+    $email = $_POST["email"];
+    // $phone = $_POST["phone"];
 
-try{
-    $pdo = new PDO($dsn, $dbusername, $dbpassword);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e){
-    echo "Connection Failed" . $e->getMessage();
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "test";
 
-}
+    // Create connection
+    $conn = mysqli_connect($host, $username, $password, $dbname);
 
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = "INSERT INTO users (firstName, lastName, email)
+        VALUES ('$firstName', '$lastName', '$email')";
+
+    // echo $sql;
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+
+?>
